4ec978d18cd1362b356c1eb662caf1b6e7fa8cec
Vadim Demedes <vdemedes@gmail.com>
2017-07-29 00:01:04 +0200

    Switch from Mocha to AVA (#17)

--- test/tcp.js
+++ test/tcp.js
@@ -1,46 +1,35 @@
-/* eslint-env mocha */
+import {spy} from 'sinon'
+import test from 'ava'
+import net from 'net'
+import Client from '..'
 
-'use strict'
+test('call', async t => {
+  const handleCall = spy((socket, data) => {
+    data = JSON.parse(data)
 
-const net = require('net')
-const assert = require('assert')
-const Client = require('..')
+    t.is(data.method, 'Foo.Bar')
 
-describe('jsonrpc2 (tcp)', function () {
-  it('should work', function * () {
-    let called = false
-
-    const server = net.createServer(socket => {
-      socket.on('data', function (buf) {
-        const json = JSON.parse(buf)
-
-        assert.equal(json.method, 'Foo.Bar')
-        called = true
-
-        socket.write(JSON.stringify({
-          id: json.id,
-          jsonrpc: '2.0',
-          result: 42
-        }) + '\n')
-      })
-    })
-
-    server.listen(4003)
-
-    const client = new Client('tcp://localhost:4003/rpc')
-    const result = yield client.call('Foo.Bar', { foo: 'bar' })
-    assert(called, 'called the method')
-    assert.equal(result, 42)
+    socket.write(JSON.stringify({
+      id: data.id,
+      jsonrpc: '2.0',
+      result: 42
+    }) + '\n')
   })
 
-  it('should handle connection errors', function * () {
-    let error = null
-    const client = new Client('tcp://sldkfjsdflksdfjsdklfjssdlfj:23423')
-    try {
-      yield client.call('Foo.Bar', 12)
-    } catch (e) {
-      error = e
-    }
-    assert(error)
+  const server = net.createServer(socket => {
+    socket.on('data', handleCall.bind(null, socket))
   })
+
+  server.listen(4003)
+
+  const client = new Client('tcp://localhost:4003')
+  const result = await client.call('Foo.Bar', { foo: 'bar' })
+
+  t.true(handleCall.calledOnce)
+  t.is(result, 42)
+})
+
+test('connection error', async t => {
+  const client = new Client('tcp://not-found:23424')
+  await t.throws(client.call('Foo.Bar', 12))
 })
